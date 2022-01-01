e881af0b8b0e5b0c1f28cffead51234c5c2a3f1b
ShihanWang <wangshihan751@gmail.com>
2022-01-01 12:15:15 +0800

    v-0.2:完成tiff打开显示

--- UI.py
+++ UI.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'UI.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
+from PyQt5.QtWidgets import *
+from RSIC.utils import Chart, PhotoViewer
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -22,7 +18,7 @@ class Ui_MainWindow(object):
         self.window_layout.setContentsMargins(0, 0, 2, 0)
         self.window_layout.setSpacing(5)
         self.window_layout.setObjectName("window_layout")
-        self.gv_img = QtWidgets.QGraphicsView(self.centralwidget)
+        self.gv_img = PhotoViewer(self.centralwidget)
         self.gv_img.setStyleSheet("background-color:white")
         self.gv_img.setObjectName("gv_img")
         self.window_layout.addWidget(self.gv_img)
@@ -43,7 +39,7 @@ class Ui_MainWindow(object):
         self.btn_open.setStyleSheet("")
         self.btn_open.setText("")
         self.btn_open.setObjectName("btn_open")
-        self.btn_open.setIcon(QIcon('asset/open.png'))
+        self.btn_open.setIcon(QIcon('asset/icon/open.png'))
         self.btn_open.clicked.connect(lambda: self.get_file(MainWindow))
 
         self.btn_layout.addWidget(self.btn_open)
@@ -57,8 +53,7 @@ class Ui_MainWindow(object):
         self.btn_export.setMaximumSize(QtCore.QSize(50, 50))
         self.btn_export.setText("")
         self.btn_export.setObjectName("btn_export")
-        self.btn_export.setIcon(QIcon('asset/export.png'))
-        self.btn_export.clicked.connect(lambda: self.test(MainWindow))
+        self.btn_export.setIcon(QIcon('asset/icon/export.png'))
 
         self.btn_layout.addWidget(self.btn_export)
         self.btn_histogram = QtWidgets.QPushButton(self.centralwidget)
@@ -71,7 +66,8 @@ class Ui_MainWindow(object):
         self.btn_histogram.setMaximumSize(QtCore.QSize(50, 50))
         self.btn_histogram.setText("")
         self.btn_histogram.setObjectName("btn_histogram")
-        self.btn_histogram.setIcon(QIcon('asset/histogram.png'))
+        self.btn_histogram.setIcon(QIcon('asset/icon/histogram.png'))
+        self.btn_histogram.clicked.connect(self.test)
 
         self.btn_layout.addWidget(self.btn_histogram)
         self.btn_classify = QtWidgets.QPushButton(self.centralwidget)
@@ -84,7 +80,7 @@ class Ui_MainWindow(object):
         self.btn_classify.setMaximumSize(QtCore.QSize(50, 50))
         self.btn_classify.setText("")
         self.btn_classify.setObjectName("btn_classify")
-        self.btn_classify.setIcon(QIcon('asset/classify.png'))
+        self.btn_classify.setIcon(QIcon('asset/icon/classify.png'))
 
         self.btn_layout.addWidget(self.btn_classify)
         self.btn_help = QtWidgets.QPushButton(self.centralwidget)
@@ -97,7 +93,7 @@ class Ui_MainWindow(object):
         self.btn_help.setMaximumSize(QtCore.QSize(50, 50))
         self.btn_help.setText("")
         self.btn_help.setObjectName("btn_help")
-        self.btn_help.setIcon(QIcon('asset/help.png'))
+        self.btn_help.setIcon(QIcon('asset/icon/help.png'))
 
         self.btn_layout.addWidget(self.btn_help)
         spacerItem = QtWidgets.QSpacerItem(10, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
@@ -110,16 +106,26 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Wsh遥感分类器"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Remote Sensing Image Classification"))
         self.btn_open.setToolTip(_translate("MainWindow", "打开文件"))
         self.btn_export.setToolTip(_translate("MainWindow", "结果另存为"))
-        self.btn_histogram.setToolTip(_translate("MainWindow", "像元直方图"))
+        self.btn_histogram.setToolTip(_translate("MainWindow", "像元统计"))
         self.btn_classify.setToolTip(_translate("MainWindow", "分类"))
         self.btn_help.setToolTip(_translate("MainWindow", "帮助"))
 
+
+
     def get_file(self, window):
-        file_name, _ = QFileDialog.getOpenFileName(window, "选择影像", r'E:\Desktop', '*.tif')
-        print(file_name)
+        self.file_name, _ = QFileDialog.getOpenFileName(window, "选择影像", r'E:\Desktop\GisFile\DEM', '*.tif;;*.jpg;;*.jpeg;;*.png')
+        if self.file_name:
+            self.gv_img.setPhoto(QPixmap(self.file_name))
+            """
+            pix = QPixmap(self.file_name)
+            item = QGraphicsPixmapItem(pix)
+            scene = QGraphicsScene()
+            scene.addItem(item)
+            self.gv_img.setScene(scene)
+            """
         """
         self.dialog = QFileDialog()
         self.dialog.setWindowTitle("选择影像")
@@ -129,7 +135,24 @@ class Ui_MainWindow(object):
             file_name = self.dialog.selectedFiles()[0]
             print(file_name)
         """
+    def test(self):
+        chart = Chart()
+        chart.show()
 
-    def test(self, window):
-        qmb = QMessageBox.critical(window, '错误', '这是一个错误对话框', QMessageBox.Ok | QMessageBox.Cancel, QMessageBox.Ok)
+class My_window(QMainWindow):
+    def __init__(self):
+        super(My_window, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.set_ui()
+        self.setStyleSheet("width:100vw;height:100vh;background-color:black")
+        self.resize(1600, 900)
+    def set_ui(self):
+        self.setWindowIcon(QIcon(r'asset/icon/sticker.png'))
 
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = My_window()
+    win.show()
+    sys.exit(app.exec_())
